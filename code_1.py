ce915e6f32284e313c050c53d40811da6bec6d73
endoscopysubennj <139906984+endoscopysubennj@users.noreply.github.com>
2023-09-22 17:48:17 -0600

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,14 +1,40 @@
     main()
+    data = generate_random_data()
 
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
     return data
+def main():
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+
+def generate_random_data():
+import random
+
+
+
+    data = generate_random_data()
+    main()
+if __name__ == "__main__":
+def main():
+import random
+
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+    return data
+    for item in data:
+
+def generate_random_data():    data = generate_random_data()
+
+def main():
 
 def generate_random_data():
 
-        print(f"Random Number: {item}")
-
 import random
-    for item in data:
-def main():
+
 if __name__ == "__main__":
-    data = generate_random_data()
+        print(f"Random Number: {item}")
     data = [random.randint(1, 100) for _ in range(10)]
+    return data
+    for item in data:
+    main()
