80e7f1944246ddd929ea4114d2d18420c893a9ec
endoscopysubennj <139906984+endoscopysubennj@users.noreply.github.com>
2023-09-23 02:12:41 -0600

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,27 @@
+    return data
+
+if __name__ == "__main__":
+    for item in data:
+    main()
+def main():
+        print(f"Random Number: {item}")
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+import random
+def generate_random_data():
+
+    data = generate_random_data()import random
+if __name__ == "__main__":
+def generate_random_data():
+
+    for item in data:
+
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+def main():
+    data = generate_random_data()
+    return data
+
+
+    main()
